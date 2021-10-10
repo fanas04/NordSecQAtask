09f4eb27c329cd0a5ebf37aff4305f1760718c58
fanas04 <64746205+fanas04@users.noreply.github.com>
2021-10-10 19:34:58 +0300

    tests

---
+++ page.py
@@ -0,0 +1,89 @@
+from time import sleep
+import random
+from element import BasePageElement
+from locators import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class BasePage(object):
+
+    def __init__(self, driver):
+        self.driver = driver
+
+
+class SearchTextElement(BasePageElement):
+    page_title = 'Clickity Click Tech Skill Task'
+
+
+class MainPage(BasePage):
+
+    def is_title_matches(self):
+        return SearchTextElement.page_title in self.driver.title
+
+    def grid_size_3_x_3(self):
+        wait = WebDriverWait(self.driver, 40)
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_2)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_3)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button2_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button2_2)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button2_3)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button3_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button3_2)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button3_3)))).click()
+
+    def grid_size_4_x_4_perimeter(self):
+        wait = WebDriverWait(self.driver, 40)
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_2)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_3)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button1_4)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button2_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button3_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button3_4)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button2_4)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button4_1)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button4_2)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button4_3)))).click()
+        wait.until(EC.visibility_of((self.driver.find_element(*GridIconLocators.Button4_4)))).click()
+
+    def alert(self, text):
+        obj = self.driver.switch_to.alert
+        obj.send_keys(text)
+        obj.accept()
+
+    def count_grid_boxes(self):
+        value = len(self.driver.find_elements_by_css_selector("div.mainGrid > div.row > div.icon"))
+        return int(value)
+
+    @staticmethod
+    def random_incorrect_symbol():
+        randomInt = ['*', '%', '/', '+']
+        grid_size = random.choice(randomInt)
+        return grid_size
+
+    @staticmethod
+    def random_correct_number():
+        randomInt = ['3', '4', '5', '6', '7', '8', '9']
+        grid_size = random.choice(randomInt)
+        return grid_size
+
+    @staticmethod
+    def random_incorrect_number():
+        randomInt = ['1', '2', '10', '999999']
+        grid_size = random.choice(randomInt)
+        return grid_size
+
+    @staticmethod
+    def dialog_notification():
+        text = "Done! Ready for the next try? Give me a size [3-9]:"
+        return text
+
+    @staticmethod
+    def dialog_notification_second():
+        text = "Not a good size!"
+        return text
+
+
---
+++ test.py
@@ -0,0 +1,74 @@
+import unittest
+import page
+from time import sleep
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from locators import *
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("file:///C:/Users/ignas.asmena/Desktop/QA_Task.html")
+
+    def test_correct_grid_size(self):
+        main_page = page.MainPage(self.driver)
+        assert main_page.is_title_matches(), "Page title doesn't match."
+        grid_size = main_page.random_correct_number()
+        if grid_size >= '3' or grid_size <= '9':
+            main_page.grid_size_4_x_4_perimeter()
+            main_page.alert(grid_size)
+            assert main_page.count_grid_boxes() == int(grid_size) * int(grid_size)
+            print("Grid size given = ", grid_size, "Test passed")
+        else:
+            print("Given incorrect grid size")
+
+    def test_incorrect_grid_size_numbers(self):
+        main_page = page.MainPage(self.driver)
+        grid_size = main_page.random_incorrect_number()
+        main_page.grid_size_4_x_4_perimeter()
+        obj = self.driver.switch_to.alert
+        assert obj.text == main_page.dialog_notification(), "Wrong notification text"
+        obj.send_keys(grid_size)
+        obj.accept()
+        assert obj.text == main_page.dialog_notification_second(), "Wrong notification text"
+        obj.accept()
+        print("Grid size given = ", grid_size, "Test passed")
+
+    def test_incorrect_grid_size_symbols(self):
+        main_page = page.MainPage(self.driver)
+        grid_size = main_page.random_incorrect_symbol()
+        main_page.grid_size_4_x_4_perimeter()
+        try:
+            obj = self.driver.switch_to.alert
+            assert obj.text == main_page.dialog_notification(), "Wrong notification text"
+            obj.send_keys(grid_size)
+            obj.accept()
+            print("Grid size given = ", grid_size, "Test passed")
+        except NoSuchElementException:
+            sleep(1)
+
+    def test_all_button_clicked(self):
+        main_page = page.MainPage(self.driver)
+        main_page.grid_size_4_x_4_perimeter()
+        main_page.alert('3')
+        main_page.grid_size_3_x_3()
+        assert self.driver.find_element(*GridIconLocators.Button1_1).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button1_2).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button1_3).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button2_1).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button2_2).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button2_3).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button3_1).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button3_2).get_attribute("active") == "true"
+        assert self.driver.find_element(*GridIconLocators.Button3_3).get_attribute("active") == "true"
+        print("All button at grid 3x3 was clicked")
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
+
